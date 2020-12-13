c8ac11489e80235295443e8ae9c652d44a226a73
Alexander Zelensky <zeeelensky@gmail.com>
2020-12-13 17:53:51 +0200

    Лабораторна №6

---
+++ lab6-c++.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	long int SumOfDivisors(long int);
+	long int n; //межа вимірювання
+	cout << "cin n pliz" << endl; //просимо ввести межу вимірювання
+	cin >> n; //зчитуємо межу вимірювання
+	for (long int i = 1; i < n; i++) {
+		long int sum = SumOfDivisors(i);
+		if (sum == i) {		//перевіряємо число на досконалість
+			cout << i << endl; //виводмио число, якщо воно досконале
+		}
+	}
+	return 0;
+}
+
+long int SumOfDivisors(long int number) { //функція для знаходження суми дільників числа
+	long int s = 0;
+	for (long int j = 1; j < number; j++) {
+		if (number % j == 0) {
+			s = s + j;		//знаходимо суму дільників числа
+		}
+	}
+	return s;
+}
---
+++ lab6-py.py
@@ -0,0 +1,12 @@
+def SumOfDivisors(number): #функція для знаходження суми дільників числа
+    s = 0
+    for j in range(1, number-1):
+        if number % j == 0:
+            s += j #знаходимо суму дільників числа
+    return s
+
+n = int(input("Enter a value of n:")) #просимо ввести межу вимірювання
+for i in range (1, n-1):
+    sum = SumOfDivisors(i)
+    if sum == i: #перевіряємо число на досконалість
+        print(i) #виводмио число, якщо воно досконале
